0ac46fa6ff6d4136e4bc91da89bd22dc3c0d0975
YanAnHuaZai <yananhuazai@163.com>
2020-09-02 06:28:00 +0800

    类型

--- src/main/java/org/huazai/service/YellowService.java
+++ src/main/java/org/huazai/service/YellowService.java
@@ -92,6 +92,7 @@ public class YellowService {
             // rha: 日韩
             // gch: 国产
             // wm: 无码
+            // omei: 欧美
             response = OkHttpKit.get("http://mu.coroad.cn/bf2.php?lx=gch&t=" + System.currentTimeMillis()).header("User-Agent", "Mozilla/5.0 (Linux; Android 10; ALP-AL00 Build/HUAWEIALP-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/79.0.3945.116 Mobile Safari/537.36 hap/1068/huawei com.huawei.fastapp/2.2.2.303 com.yunshuo.cloudbrowser/8.5.1 ({\"packageName\":\"com.tencent.mobileqq\",\"type\":\"other\",\"extra\":\"{}\"})").execute();
         } catch (IOException e) {
             e.printStackTrace();
