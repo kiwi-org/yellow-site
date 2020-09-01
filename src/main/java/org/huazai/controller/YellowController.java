df7b57f4a50d3c09e42948e04b9553521fd50eb8
YanAnHuaZai <yananhuazai@163.com>
2020-09-02 04:40:29 +0800

    获取具体的内容

--- src/main/java/org/huazai/controller/YellowController.java
+++ src/main/java/org/huazai/controller/YellowController.java
@@ -1,11 +1,32 @@
 package org.huazai.controller;
 
+import org.huazai.service.YellowService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author YanAnHuaZai
  * created at：2020-09-01 22:22
  */
-@Controller
+@RestController
+@RequestMapping("yellow")
 public class YellowController {
+
+    @Resource
+    private YellowService yellowService;
+
+    /**
+     * 获取具体的内容
+     * @author YanAnHuaZai
+     * create 2020年09月02日04:40:00
+     * @param title 标题
+     */
+    @RequestMapping("{title}")
+    public String content(@PathVariable String title) {
+        return yellowService.queryContentByTitle(title);
+    }
 }
