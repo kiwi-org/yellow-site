0ccf31830031eacf36c656ee4a8a64b3f0903872
YanAnHuaZai <yananhuazai@163.com>
2020-09-02 04:22:37 +0800

    查询所有的标题

--- src/main/java/org/huazai/mapper/SiteCollectMapper.java
+++ src/main/java/org/huazai/mapper/SiteCollectMapper.java
@@ -1,11 +1,24 @@
 package org.huazai.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
 import org.huazai.model.entity.SiteCollectDO;
+
+import java.util.List;
 
 /**
  * @author YanAnHuaZai
  * created at：2020-09-02 01:06
  */
 public interface SiteCollectMapper extends BaseMapper<SiteCollectDO> {
+
+    /**
+     * 查询所有的标题
+     * @author YanAnHuaZai
+     * create 2020年09月02日04:22:25
+     * @return 标题集合
+     */
+    @Select("select title from t_site_collect")
+    List<String> selectTitleList();
+
 }
