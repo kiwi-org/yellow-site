3f0d7321f76b055af779f0ca9821c7f078c8463e
YanAnHuaZai <yananhuazai@163.com>
2020-09-01 22:14:17 +0800

    first commit

---
+++ src/main/java/org/huazai/Application.java
@@ -0,0 +1,15 @@
+package org.huazai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YanAnHuaZai
+ * created at：2020-09-01 22:10
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
