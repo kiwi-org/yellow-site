78c83a2025ed7c107988071b9f60ec8129a5ccd3
YanAnHuaZai <yananhuazai@163.com>
2020-09-02 00:45:17 +0800

    网站汇总数据

---
+++ src/main/java/org/huazai/model/SiteCollectDO.java
@@ -0,0 +1,21 @@
+package org.huazai.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 网站汇总
+ * @author YanAnHuaZai
+ * created at：2020-09-02 00:44
+ */
+@Getter
+@Setter
+@ToString
+public class SiteCollectDO {
+
+    private String title;
+    private String url;
+    private String content;
+
+}
